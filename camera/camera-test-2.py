cce20e88da5b6e7c768b2d34e5067f8cf7c08c66
Mejdi Thabet <mejdi.thabet@polytechnicien.tn>
2022-01-23 18:07:51 +0100

    Add file

--- camera/camera-test-2.py
+++ camera/camera-test-2.py
@@ -2,6 +2,12 @@ import cv2
 
 # initialize the camera
 cam = cv2.VideoCapture(0)
+
+# set the format into MJPG in the FourCC format 
+cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
+
+# cam.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('J','P','E','G'))
+
 ret, image = cam.read()
 
 if ret:
--- camera/camera-test.py
+++ camera/camera-test.py
@@ -1,6 +1,12 @@
 import cv2      					        # import OpenCv lib
 cap = cv2.VideoCapture(0)            	    # create cap object 
 
+# set the format into MJPG in the FourCC format 
+cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('M','J','P','G'))
+
+# cap.set(cv2.CAP_PROP_FOURCC,cv2.VideoWriter_fourcc('J','P','E','G'))
+
+
 if not cap.isOpened():                     	# check if the camera is opened
     print('Cannot open webcam')
 else:
